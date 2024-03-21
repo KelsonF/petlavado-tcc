c16a905c6546593390663784c112ec2e805be1cb
Kelson Eduardo <kfeduardo2003@gmail.com>
2024-03-20 20:50:13 -0700

    Initial project setup

---
+++ src/main/java/br/com/backend/petlavado/petlavado/PetLavadoApplication.java
@@ -0,0 +1,13 @@
+package br.com.backend.petlavado.petlavado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetLavadoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetLavadoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/backend/petlavado/petlavado/PetLavadoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.backend.petlavado.petlavado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetLavadoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

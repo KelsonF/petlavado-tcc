6b0875cb40475f04c4d9d6d81a3999fb035f33ce
Kelson Eduardo <kfeduardo2003@gmail.com>
2024-04-08 18:05:25 -0700

    refactor: improve the Product entity

--- src/main/java/br/com/backend/petlavado/petlavado/modules/products/domain/entities/Product.java
+++ src/main/java/br/com/backend/petlavado/petlavado/modules/products/domain/entities/Product.java
@@ -2,24 +2,34 @@ package br.com.backend.petlavado.petlavado.modules.products.domain.entities;
 
 import br.com.backend.petlavado.petlavado.modules.user.domain.entities.Store;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
 @Entity
+@AllArgsConstructor
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @NotEmpty
     private String description;
+
+    @NotNull
+    @Positive
     private Double value;
+
     private String imageUrl;
+
     @ManyToOne
     private Store store;
+
+    public Product() {
+    }
 }

2978a375845e650a4ca6863ec2103740578f7dd5
Kelson Eduardo <kfeduardo2003@gmail.com>
2024-04-11 15:20:14 -0400

    feat: add Client and Store entities

--- src/main/java/br/com/backend/petlavado/petlavado/modules/user/domain/entities/Client.java
+++ src/main/java/br/com/backend/petlavado/petlavado/modules/user/domain/entities/Client.java
@@ -1,20 +1,18 @@
 package br.com.backend.petlavado.petlavado.modules.user.domain.entities;
 
-import br.com.backend.petlavado.petlavado.modules.products.domain.entities.Product;
-import jakarta.persistence.*;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.List;
-
 @Getter
 @Setter
-@Entity
 @NoArgsConstructor
-public class Store {
+public class Client {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
@@ -37,16 +35,13 @@ public class Store {
 
     @NotBlank(message = "The cnpj cannot be empty")
     @NotNull
-    private String cnpj;
+    private String cpf;
 
-    @OneToMany
-    private List<Product> products;
-
-    public Store(String name, String description, String email, String phoneNumber, String cnpj) {
+    public Client(String name, String description, String email, String phoneNumber, String cpf) {
         this.name = name;
         this.description = description;
         this.email = email;
         this.phoneNumber = phoneNumber;
-        this.cnpj = cnpj;
+        this.cpf = cpf;
     }
 }

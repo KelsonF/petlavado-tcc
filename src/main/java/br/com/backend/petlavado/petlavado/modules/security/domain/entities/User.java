71c256066399541d560b53790e3f967bcb6085de
KelsonF <kfeduardo2003@gmail.com>
2024-04-25 02:10:47 -0300

    refactor: update User entity

--- src/main/java/br/com/backend/petlavado/petlavado/modules/security/domain/entities/User.java
+++ src/main/java/br/com/backend/petlavado/petlavado/modules/security/domain/entities/User.java
@@ -14,6 +14,7 @@ import java.util.UUID;
 @Setter
 @NoArgsConstructor
 @Inheritance(strategy = InheritanceType.JOINED)
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)

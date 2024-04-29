a66d4e74cd5ac1e05a8a6cf7d4502a27464d7ff9
KelsonF <kfeduardo2003@gmail.com>
2024-04-29 13:16:41 -0300

    refactor: add new dependencies

--- src/main/java/br/com/backend/petlavado/petlavado/PetLavadoApplication.java
+++ src/main/java/br/com/backend/petlavado/petlavado/PetLavadoApplication.java
@@ -3,7 +3,9 @@ package br.com.backend.petlavado.petlavado;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController("/api")
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class PetLavadoApplication {
 

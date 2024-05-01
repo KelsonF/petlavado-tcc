1ec5900f439cdbb653bb03c09e1c22cfb0a2d083
KelsonF <kfeduardo2003@gmail.com>
2024-05-01 13:36:43 -0300

    refactor: remove unused imports

--- src/main/java/br/com/backend/petlavado/petlavado/modules/security/api/AuthController.java
+++ src/main/java/br/com/backend/petlavado/petlavado/modules/security/api/AuthController.java
@@ -5,7 +5,6 @@ import java.net.URI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,9 +23,7 @@ import jakarta.validation.Valid;
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
-  @Autowired
-  private AuthenticationManager authenticationManager;
-
+  
   private final AuthService authService;
   private final UserRepository userRepository;
 
